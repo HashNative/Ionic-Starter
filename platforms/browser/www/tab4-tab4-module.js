c025081132efe3a3550de19861185a148875d758
IlhamSafeek <ilhamsafeek@yahoo.com>
2019-03-12 16:08:29 +0530

    first commit

---
+++ platforms/browser/www/tab4-tab4-module.js
@@ -0,0 +1,111 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["tab4-tab4-module"],{
+
+/***/ "./src/app/tab4/tab4.module.ts":
+/*!*************************************!*\
+  !*** ./src/app/tab4/tab4.module.ts ***!
+  \*************************************/
+/*! exports provided: Tab4PageModule */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab4PageModule", function() { return Tab4PageModule; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_common__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/common */ "./node_modules/@angular/common/fesm5/common.js");
+/* harmony import */ var _angular_forms__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @angular/forms */ "./node_modules/@angular/forms/fesm5/forms.js");
+/* harmony import */ var _angular_router__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @angular/router */ "./node_modules/@angular/router/fesm5/router.js");
+/* harmony import */ var _ionic_angular__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @ionic/angular */ "./node_modules/@ionic/angular/dist/fesm5.js");
+/* harmony import */ var _tab4_page__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./tab4.page */ "./src/app/tab4/tab4.page.ts");
+
+
+
+
+
+
+
+var routes = [
+    {
+        path: '',
+        component: _tab4_page__WEBPACK_IMPORTED_MODULE_6__["Tab4Page"]
+    }
+];
+var Tab4PageModule = /** @class */ (function () {
+    function Tab4PageModule() {
+    }
+    Tab4PageModule = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["NgModule"])({
+            imports: [
+                _angular_common__WEBPACK_IMPORTED_MODULE_2__["CommonModule"],
+                _angular_forms__WEBPACK_IMPORTED_MODULE_3__["FormsModule"],
+                _ionic_angular__WEBPACK_IMPORTED_MODULE_5__["IonicModule"],
+                _angular_router__WEBPACK_IMPORTED_MODULE_4__["RouterModule"].forChild(routes)
+            ],
+            declarations: [_tab4_page__WEBPACK_IMPORTED_MODULE_6__["Tab4Page"]]
+        })
+    ], Tab4PageModule);
+    return Tab4PageModule;
+}());
+
+
+
+/***/ }),
+
+/***/ "./src/app/tab4/tab4.page.html":
+/*!*************************************!*\
+  !*** ./src/app/tab4/tab4.page.html ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "<ion-header>\n  <ion-toolbar>\n    <ion-title>Accounts</ion-title>\n  </ion-toolbar>\n</ion-header>\n\n<ion-content padding>\n  <div class=\"list\" style=\"margin-bottom:0\">\n    <div class=\"card\">\n      <div class=\"item item-text-wrap\">\n        <h1 class=\"box-blue\">Hi</h1>\n        <p class=\"small thin\">\n          Are you looking for <b>More</b> Apps?.. Don't worry, You can access our sources from following ways. Help us to do more\n        </p>\n      </div>\n    </div>\n\n  </div>\n\n  <div class=\"card\">\n    <a class=\"item item-text-wrap\" ng-href=\"mailto:deshatom@gmail.com?Subject=Custom App Request\" target=\"_top\">\n      <h2 class=\"box-blue\">For Business</h2>\n      <p class=\"small thin\">\n        If you need <b>Cus</b><span class=\"thin\">tom</span> <b>App</b> <span class=\"thin small\">(with or without backend)</span>. Feel Free to drop an <b>Email</b>\n      </p>\n      <p class=\"email-text\">deshatom@gmail.com</p>\n    </a>\n  </div>\n  <div class=\"card\">\n    <div class=\"item item-text-wrap thin \">\n      <p class=\"small\">\n        Specialized in Ionic, Angular, React, IOS, Android <b>React-Native</b>, Nodejs Meteror and etc...\n      </p>\n    </div>\n  </div>\n</ion-content>\n"
+
+/***/ }),
+
+/***/ "./src/app/tab4/tab4.page.scss":
+/*!*************************************!*\
+  !*** ./src/app/tab4/tab4.page.scss ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = "\n/*# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiJzcmMvYXBwL3RhYjQvdGFiNC5wYWdlLnNjc3MifQ== */"
+
+/***/ }),
+
+/***/ "./src/app/tab4/tab4.page.ts":
+/*!***********************************!*\
+  !*** ./src/app/tab4/tab4.page.ts ***!
+  \***********************************/
+/*! exports provided: Tab4Page */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "Tab4Page", function() { return Tab4Page; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+
+
+var Tab4Page = /** @class */ (function () {
+    function Tab4Page() {
+    }
+    Tab4Page.prototype.ngOnInit = function () {
+    };
+    Tab4Page = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Component"])({
+            selector: 'app-tab4',
+            template: __webpack_require__(/*! ./tab4.page.html */ "./src/app/tab4/tab4.page.html"),
+            styles: [__webpack_require__(/*! ./tab4.page.scss */ "./src/app/tab4/tab4.page.scss")]
+        }),
+        tslib__WEBPACK_IMPORTED_MODULE_0__["__metadata"]("design:paramtypes", [])
+    ], Tab4Page);
+    return Tab4Page;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=tab4-tab4-module.js.map
